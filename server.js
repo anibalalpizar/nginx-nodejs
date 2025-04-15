279558180815e53d4e11a355f939416df761b73f
anibalalpizar <anibal.alpizar14@gmail.com>
2025-04-14 23:25:34 -0600

    Set up static file serving for images and serve index.html on root route

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+    console.log(`Request received: ${req.method} ${req.url}`);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
